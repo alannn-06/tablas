e803c4d5a1e0508e620135c598f11b4f527a3cf9
alannn-06 <alanchanatasig2006@gmail.com>
2026-04-27 18:43:01 -0500

    Funcionamiento de cualquier tabla de mutiplicar con diseño desplegado

--- multiplicar.js
+++ multiplicar.js
@@ -1,10 +1,23 @@
-function generarTablas(){ 
+function generarTablas() {
+    
     let contenedor = document.getElementById("contenedorTabla");
-    contenedor.innerHTML = "<h1>PROBANDO</h1>"
+    let input = document.getElementById("inputNumero");
+    let tituloTabla = document.getElementById("nombreTabla");
+    
+    let numero = parseInt(input.value);
+
+    if (isNaN(numero)) {
+        alert("Por favor, escribe un número primero ");
+        return;
+    }
+
+    tituloTabla.innerText = "Tabla del " + numero;
 
     let contenido = "";
-    for(let i=1; i<=10; i++){
-        contenido = contenido +"<p>"+"5 x "+ i +" = "+(5*i) +"</p>"
+    
+    for (let i = 1; i <= 10; i++) {
+        contenido += `<div class="fila"><span>${numero} x ${i}</span><span>=</span><span>${numero * i}</span></div>`;
     }
+
     contenedor.innerHTML = contenido;
-} 
+}
