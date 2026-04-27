de34e8267ab0cab87027a6c31d88e5d2888a114a
alannn-06 <alanchanatasig2006@gmail.com>
2026-04-27 18:19:02 -0500

    Generación de la tabla del 5 con el boton

--- multiplicar.js
+++ multiplicar.js
@@ -3,8 +3,10 @@ function generarTablas(){
     contenedor.innerHTML = "<h1>PROBANDO</h1>"
 
     let contenido = "";
+    let nombreTabla = document.getElementById("nombreTabla");
+    nombreTabla.innerHTML = "<h1>TABLA DEL 5</h1>";
     for(let i=1; i<=10; i++){
-        contenido = contenido +"<p>"+"3 x "+ i +" = "+(3*i) +"</p>"
+        contenido = contenido +"<p>"+"5 x "+ i +" = "+(5*i) +"</p>"
     }
     contenedor.innerHTML = contenido;
 } 
