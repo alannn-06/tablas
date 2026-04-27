b69e99183d21f85d563a339680452c81b6a40230
alannn-06 <alanchanatasig2006@gmail.com>
2026-04-27 18:36:34 -0500

    caja de texto incluido para interacruar con el usuario

--- multiplicar.js
+++ multiplicar.js
@@ -3,8 +3,6 @@ function generarTablas(){
     contenedor.innerHTML = "<h1>PROBANDO</h1>"
 
     let contenido = "";
-    let nombreTabla = document.getElementById("nombreTabla");
-    nombreTabla.innerHTML = "<h1>TABLA DEL 5</h1>";
     for(let i=1; i<=10; i++){
         contenido = contenido +"<p>"+"5 x "+ i +" = "+(5*i) +"</p>"
     }
